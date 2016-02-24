eeb8577bf0aaeb1ea1b9632b4cb6f35a55359e9e
A.Khotoolev <akhotoolev@gmail.com>
2016-02-24 14:12:57 +0300

    Add Utils

---
+++ Contacts.Util/Program.cs
@@ -0,0 +1,18 @@
+﻿using Contacts.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Util {
+    class Program {
+        static void Main(string[] args) {
+            var repository = new PersonRepository();
+            foreach(var item in repository.GetAll()) {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
