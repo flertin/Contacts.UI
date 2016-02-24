0d272cd610eadb6576c1ffc16348b9c4ad52661d
A.Khotoolev <akhotoolev@gmail.com>
2016-02-24 15:06:44 +0300

    Add unit tests

---
+++ Contacts.DAL/FileRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.DAL {
+    public class FileRepository : IPersonRepository {
+        private string _fileName;
+        public FileRepository(string fileName) {
+            _fileName = fileName;
+        }
+
+        public void Add(Person item) {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetAll() {
+            using(var ) {
+
+            }
+        }
+
+        public void Remove(int index) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Contacts.Tests/FileRepositoryTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Contacts.DAL;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Contacts.Tests {
+    [TestClass]
+    public class FileRepositoryTest {
+        [TestMethod]
+        public void TestFileRepositoryIsIRepository() {
+            var repository = new FileRepository();
+            var result = repository is IPersonRepository;
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestAllDataLoaded() {
+            var sb = new StringBuilder();
+            sb.AppendLine("Name1");
+            sb.AppendLine("Org1");
+            sb.AppendLine("Name2");
+            sb.AppendLine("Org2");
+            var fileName = Path.Combine(Path.GetTempPath(), "test.data");
+            File.WriteAllText(fileName, sb.ToString());
+            //------------------------
+
+            var expected = new List<Person> {
+                new Person {
+                    Name="Name1",
+                    Organization="Org1"
+                },
+                new Person {
+                    Name="Name2",
+                    Organization="Org2"
+                }
+            };
+
+            var repository = new FileRepository();
+            var items = repository.GetAll();
+
+            Assert.AreEqual(expected.Count, items.Count);
+            for(int i = 0; i < expected.Count; i++) {
+                var exp = expected[i];
+                var actual = items[i];
+                Assert.AreEqual(exp.Name, actual.Name);
+                Assert.AreEqual(exp.Organization, actual.Organization);
+            }
+
+
+            //------------------------
+            File.Delete(fileName);
+        }
+    }
+}
